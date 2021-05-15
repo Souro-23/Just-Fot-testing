771927341a06b873db192315e8c039f67be5498b
souro12345 <52583823+souro12345@users.noreply.github.com>
2021-05-15 19:37:52 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import firebase from 'firebase/app';
+import "firebase/auth";
+
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA8zHuUzK0S_rv7y91DF9f2hEKIY_QIdas",
+    authDomain: "corded-cinema-283913.firebaseapp.com",
+    projectId: "corded-cinema-283913",
+    storageBucket: "corded-cinema-283913.appspot.com",
+    messagingSenderId: "814903168271",
+    appId: "1:814903168271:web:c4519b95f61ad56c6c027a"
+  };
+firebase.initializeApp(firebaseConfig)
+
+
+const auth = firebase.auth()
 
 function App() {
+  auth.onAuthStateChanged(user=>{
+  console.log(user)
+  console.log(localStorage.getItem("expirationDate"))
+  console.log(localStorage.getItem("r_expirationDate"))
+})
+console.log("helo")
+
   return (
     <div className="App">
       <header className="App-header">
